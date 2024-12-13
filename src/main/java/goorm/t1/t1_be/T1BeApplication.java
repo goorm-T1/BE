6f4de3e95de436ec5b3c2da594c8c62aea1a00fd
WZNT-KimHyeJin <pcrs5278@naver.com>
2024-12-13 21:31:10 +0900

    init : 프로젝트 생성

---
+++ src/main/java/goorm/t1/t1_be/T1BeApplication.java
@@ -0,0 +1,13 @@
+package goorm.t1.t1_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T1BeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(T1BeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/goorm/t1/t1_be/T1BeApplicationTests.java
@@ -0,0 +1,13 @@
+package goorm.t1.t1_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T1BeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
