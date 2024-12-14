5e9c4cf1e8b62838093b8fbe03fd1921a3695467
WZNT-KimHyeJin <pcrs5278@naver.com>
2024-12-14 22:49:34 +0900

    feat : CorsConfig handler 구현

---
+++ src/main/java/goorm/t1/t1_be/global/config/CorsConfig.java
@@ -0,0 +1,13 @@
+package goorm.t1.t1_be.global.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowCredentials(true);
+    }
+}
